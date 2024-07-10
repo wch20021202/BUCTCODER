1baaf5ff99c041f90ffaec57e7caad71f340ad13
Richard601 <96051507+Richard601@users.noreply.github.com>
2024-07-10 10:19:49 +0800

    Add files via upload

---
+++ demo1/pachong/src/main/java/org/example/pachong/PachongApplication.java
@@ -0,0 +1,13 @@
+package org.example.pachong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PachongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PachongApplication.class, args);
+    }
+
+}
---
+++ demo1/pachong/src/test/java/org/example/pachong/PachongApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.pachong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PachongApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
